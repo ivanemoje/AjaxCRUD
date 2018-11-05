50fab05606b7086885fedfbb42afcd39ff1c2712
Ivan Emoje <emojeivan@gmail.com>
2018-11-05 10:52:21 +0300

    change urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,8 +5,8 @@ from mysite.books import views
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
-    url(r'^books/$', views.book_list, name='book_list'),
-    url(r'^books/create/$', views.book_create, name='book_create'),
-    url(r'^books/(?P<pk>\d+)/update/$', views.book_update, name='book_update'),
-    url(r'^books/(?P<pk>\d+)/delete/$', views.book_delete, name='book_delete'),
+    url(r'^clients/$', views.book_list, name='book_list'),
+    url(r'^clients/create/$', views.book_create, name='book_create'),
+    url(r'^clients/(?P<pk>\d+)/update/$', views.book_update, name='book_update'),
+    url(r'^clients/(?P<pk>\d+)/delete/$', views.book_delete, name='book_delete'),
 ]
